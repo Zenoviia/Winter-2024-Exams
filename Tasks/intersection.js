af115dd9118672e166fad24e051f0c5e5c774ce3
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 00:28:32 +0200

    6-difference-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,18 +1,21 @@
 // Find an intersection of two dictionaries
 
-// Step 5
-// Add 'use strict';
+// Step 6
+// Use forEach to iterate  the keys
+// Use the arrow function
+
+'use strict';
 
 'use strict';
 
 const intersection = function intersection(object1, object2) {
   const firstKeys = Object.keys(object1);
 
-  for (const attributeName of firstKeys) {
-    if (object1[attributeName] !== object2[attributeName]) {
-      delete object1[attributeName];
+  firstKeys.forEach((key) => {
+    if (object1[key] !== object2[key]) {
+      delete object1[key];
     }
-  }
+  });
 
   return object1;
 };
