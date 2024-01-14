97d659a9ffc20d21168c3012253a5db5127e7285
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 00:10:07 +0200

    1-difference-filter.js

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,22 @@
 // Filter array by type name
 
-Filter = (T, t) => {
+// Step 1
+// Change Filter to FilterByType
+// Change T to array
+// Change t to filterType
+// Change C to currentElement
+// Change x to currentIndex
+
+FilterByType = (array, filterType) => {
   remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
-      remove.unshift(x);
+  for (currentElement of array) {
+    currentIndex = array.indexOf(currentElement);
+    if (typeof array[currentIndex] !== filterType) {
+      remove.unshift(currentIndex);
     }
   }
-  for (x of remove) T.splice(x, 1);
-  return T;
+  for (currentIndex of remove) array.splice(currentIndex, 1);
+  return array;
 };
 
-module.exports = Filter;
+module.exports = FilterByType;
