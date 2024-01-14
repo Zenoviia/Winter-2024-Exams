1c9b6917301eef7c573b5b45f6a1640f8a69032b
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 00:23:38 +0200

    3-difference-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,14 +1,13 @@
 // Find an intersection of two dictionaries
 
-// Step 2
-// Declare constants
-// Get rid of unreachable code antipattern
+// Step 3
+// Replace === with !== since we're checking for unequal values to remove keys
 
 const intersection = function intersection(object_1, object_2) {
   const first_keys = Object.keys(object_1);
 
   for (const attribute_name of first_keys) {
-    if (object_1[attribute_name] === object_2[attribute_name]) {
+    if (object_1[attribute_name] !== object_2[attribute_name]) {
       delete object_1[attribute_name];
     }
   }
