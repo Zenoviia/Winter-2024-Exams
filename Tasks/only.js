b4fd77165862f6d3a6bfe593f3e3ade5456a065a
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 01:07:10 +0200

    2-difference-only.js

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,14 +1,13 @@
 // Copy only listed values from dict
 
-// Step 1
-// Add 'use strict'
-// Add const
+// Step 2
+// Use Object.keys correctly
+// Remove empty brackets [];
 
 'use strict';
 
 const only = (W, ...only) => {
-  [];
-  const X = Object.keys(W, 'a', 'b', 'c');
+  const X = Object.keys(W);
   X.forEach((Z) => {
     if (only.includes(Z)) {
       delete W[Z];
