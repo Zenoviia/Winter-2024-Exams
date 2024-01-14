440bba2d346ad54d514b37ae3431aba4feff2450
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 01:28:52 +0200

    2-difference-sample.js

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,7 +1,9 @@
 // Get one random element from an array
 
-// Step 1
-// Change SAMPLE to sample
+// Step 2
+// Add 'use strict';
+
+'use strict';
 
 const sample = (arr) => {
   arr = arr[Math.floor(Math.random() * arr.length)];
