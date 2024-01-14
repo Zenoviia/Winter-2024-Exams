8d1df817625a1e97199445cd254ad52ae25d88c4
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 00:01:18 +0200

    1-difference-duplicate.js

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,13 +1,18 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
+// Step 1
+// Change variable's name from "res" to "result"
+// Change "i" to "index"
+// Change "N" to "numberOfElements"
+
+duplicate = (value, numberOfElements) => {
+  if (numberOfElements <= 0) return [];
   else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
+    result = [];
+    for (let index = 0; index < numberOfElements; index++) {
+      result[index] = value;
     }
-    return res;
+    return result;
   }
 };
 
