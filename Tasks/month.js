6f02715db516a7f029abd2bb3d1201fca2437bb9
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 01:02:00 +0200

    3-difference-month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,8 +1,9 @@
 // Get month number
 
-// Step 2
-// Add 'use strict';
-// Add const
+// Step 3
+// Use findIndex to find the month index
+// Save the string in lower case by creating the inputLowerCase variable
+// use the ternary operator instead of if-else
 
 'use strict';
 
@@ -18,15 +19,13 @@ const months = [
   'sep',
   'oct',
   'nov',
-  'dec',
+  'dec'
 ];
 
-const monthNumber = (s) => {
-  const l = months.length;
-  for (let i = 0; i < l; i++) {
-    if (s.toLowerCase().startsWith(months[i])) return i + 1;
-  }
-  return -1;
+const monthNumber = (input) => {
+  const inputLowerCase = input.toLowerCase();
+  const index = months.findIndex(month => inputLowerCase.startsWith(month));
+  return index !== -1 ? index + 1 : -1;
 };
 
 module.exports = monthNumber;
