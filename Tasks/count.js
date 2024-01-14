b6b8f8dfc659ffa91fb66d6ebcafc14accb1f9dd
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-14 23:34:22 +0200

    5-difference-count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,20 +1,19 @@
 // Sum all number values in dict
 
-// Step 4
-// Change the global variable "sum" to a local variable by adding "let"
+// Step 5
+// Iterate directly over values using Object.values(obj)
 
 'use strict';
 
 const sumNumbers = (obj) => {
   let sum = 0;
-  
-  const keys = Object.keys(obj);
-  keys.forEach((key) => {
-   
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
+
+  Object.values(obj).forEach(value => {
+    if (typeof value === 'number') {
+      sum += value;
+    }
   });
- 
+
   return sum;
 };
 
