bdf585484dc161cf75ea894e4570dfa39c493dbd
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 00:12:27 +0200

    2-difference-filter.js

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,21 +1,22 @@
 // Filter array by type name
 
-// Step 1
-// Change Filter to FilterByType
-// Change T to array
-// Change t to filterType
-// Change C to currentElement
-// Change x to currentIndex
+// Step 2
+// Add 'use strict';
+// Declare constants using const
 
-FilterByType = (array, filterType) => {
-  remove = [];
-  for (currentElement of array) {
-    currentIndex = array.indexOf(currentElement);
+'use strict';
+
+const FilterByType = (array, filterType) => {
+  const remove = [];
+  for (const currentElement of array) {
+    const currentIndex = array.indexOf(currentElement);
     if (typeof array[currentIndex] !== filterType) {
       remove.unshift(currentIndex);
     }
   }
-  for (currentIndex of remove) array.splice(currentIndex, 1);
+  for (const currentIndex of remove) {
+    array.splice(currentIndex, 1);
+  }
   return array;
 };
 
