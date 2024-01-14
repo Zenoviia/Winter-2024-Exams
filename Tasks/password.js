ab89f34ca06badea3fb7823be283bfdfef16f422
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 01:23:23 +0200

    4-difference-password.js

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,13 +1,14 @@
 // Generate random password
 
-// Step 3
-// "key = key + alphabet[index];" "replace with key += alphabet[index];"
+// Step 4
+// Add an empty line for readability
 
 'use strict';
 
 let generatePassword = (alphabet, length) => {
   const alphabetLength = alphabet.length;
   let key = '';
+
   for (let i = 0; i < length; i++) {
     let index = Math.floor(Math.random() * alphabetLength);
     key += alphabet[index];
