794cebd9cd09e2c9c3e499ac265a07250b7d903c
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-14 23:29:36 +0200

    3-difference-count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,22 +1,20 @@
 // Sum all number values in dict
 
-// Step 2
-// Add 'use strict';
-// Add const
+// Step 3
+// Remove unnecessary brackets (), {} and []
 
 'use strict';
 
 const sumNumbers = (obj) => {
   sum = 0;
-  {
-  }
+  
   const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
+   
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
+ 
   return sum;
 };
 
