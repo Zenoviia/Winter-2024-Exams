4fa70817593a8009ad707fb2e19a2c0e8132136e
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 00:22:45 +0200

    2-difference-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,19 +1,18 @@
 // Find an intersection of two dictionaries
 
-// Step 1
-// Remove extra {}
+// Step 2
+// Declare constants
+// Get rid of unreachable code antipattern
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
- 
-  for (attribute_name of first_keys) {
+const intersection = function intersection(object_1, object_2) {
+  const first_keys = Object.keys(object_1);
+
+  for (const attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];
-     
-    } else {
       delete object_1[attribute_name];
     }
   }
+
   return object_1;
 };
 
