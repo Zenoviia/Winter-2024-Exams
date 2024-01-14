d47a0bd55adcf8148a1f6c2fd77c5f2c2e6db6b5
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 00:25:17 +0200

    4-difference-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,18 +1,21 @@
 // Find an intersection of two dictionaries
 
-// Step 3
-// Replace === with !== since we're checking for unequal values to remove keys
+// Step 4
+// Change object_1 to object1
+// Change object_2 on object2
+// Change first_keys on firstKeys
+// Change attribute_name on attributeName
 
-const intersection = function intersection(object_1, object_2) {
-  const first_keys = Object.keys(object_1);
+const intersection = function intersection(object1, object2) {
+  const firstKeys = Object.keys(object1);
 
-  for (const attribute_name of first_keys) {
-    if (object_1[attribute_name] !== object_2[attribute_name]) {
-      delete object_1[attribute_name];
+  for (const attributeName of firstKeys) {
+    if (object1[attributeName] !== object2[attributeName]) {
+      delete object1[attributeName];
     }
   }
 
-  return object_1;
+  return object1;
 };
 
 module.exports = intersection;
