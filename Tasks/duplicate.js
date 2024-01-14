ca8a0b48f1731ed22f2727e8b0535fa256fa393c
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-15 00:06:12 +0200

    4-difference-duplicate.js

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,15 +1,12 @@
 // Return an array without duplicates
 
-// Step 3
-// Simplify the if-else construct that checks if numberOfElements is less than zero by using a ternary operator
+// Step 4
+// Replace the for loop with Array.fill()
 
 'use strict';
 
 const duplicate = (value, numberOfElements) => {
-  const result = numberOfElements <= 0 ? [] : [];
-  for (let index = 0; index < numberOfElements; index++) {
-    result[index] = value;
-  }
+  const result = numberOfElements <= 0 ? [] : Array(numberOfElements).fill(value);
   return result;
 };
 
