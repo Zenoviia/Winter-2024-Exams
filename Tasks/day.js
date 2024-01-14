923a86b505cb2b78744e5af6a07073495132fe75
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2024-01-14 23:52:05 +0200

    4-difference-day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,20 +1,15 @@
 // Get day number
 
-// Step 3
-// Change let to const for the days array
+// Step 4
+// Change the for loop to Array.findIndex
 
-' use strict ';
+'use strict';
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 let parseDay = (string) => {
-  let index;
-  for (index = 0; index < days.length; index++) {
-    if (string.startsWith(days[index].toLowerCase())) {
-      return index + 1;
-    }
-  }
-  return -1;
+  const index = days.findIndex(day => string.startsWith(day.toLowerCase()));
+  return index !== -1 ? index + 1 : -1;
 };
 
 module.exports = parseDay;
